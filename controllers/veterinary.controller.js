eb1a53c95f3dd41d91216b3c59198aef4d735ad9
JeanMarko173 <jeanmarko.13.7@gmail.com>
2022-02-03 09:29:40 -0500

    Fix API registerVeterinary

--- controllers/veterinary.controller.js
+++ controllers/veterinary.controller.js
@@ -1,5 +1,6 @@
 const VeterinaryServices = require('../services/veterinary.services');
 const { validationResult } = require('express-validator');
+const veterinaryImage = `https://cdn.redcanina.es/wp-content/uploads/2019/03/26190114/baño-perro.jpg`
 
 const registerVeterinary = async (req, res, next) => {
   const errors = validationResult(req);
@@ -11,8 +12,9 @@ const registerVeterinary = async (req, res, next) => {
       });
   } else {
     const { name, detail, location, services } = req.body;
+    const avatar_url = req.body.avatar_url ? req.body.avatar_url : veterinaryImage;
     try {
-      const newVeterinary = await VeterinaryServices.register({ name, detail, location, services });
+      const newVeterinary = await VeterinaryServices.register({ name, detail, location, services, avatar_url });
       res.status(201).json({
           message: 'The veterinary was successfully registered',
           status: 'OK',
